126786ceeb280465cafa0e23a3f533aff1414b3c
Tim Callies <call3675@stthomas.edu>
2020-03-11 16:10:11 -0500

    Complete assignment

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,20 @@ function CalculateCompoundTransform(transforms) {
     // note `transform[0]` is first tranform to apply to vertex
     
     // if only one transform, set compound transform eequal to it
+    if(transforms.length === 1) {
+        compound_transform = transforms[0].mat4x4;
+    }
+
     // otherwise multiply all matrices together (in proper order)
     // `compound_transform = Matrix.multiply(...)`
-    var tranform_matrices = [];
-
-    compound_transform = new Matrix(4, 4); // change / remove this
-
+    else {
+        var transform_matrices = [];
+        for (let transform of transforms) {
+            transform_matrices.unshift(transform.mat4x4);
+        }
+        compound_transform = Matrix.multiply(transform_matrices); // change / remove this
+    }
+    
     return compound_transform;
 }
 
@@ -20,7 +28,8 @@ function CalculateTransformedVertex(vertex) {
     // multiple vertex by compound_transform
     // `final_vertex = Matrix.multiply(...)`
     var final_vertex = new Vector(4); // change / remove this
-
+    console.log(vertex, compound_transform)
+    final_vertex = Matrix.multiply([compound_transform, vertex]);
     return final_vertex;
 }
 
@@ -28,6 +37,26 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
+    switch(type) {
+        case "translate":
+            Mat4x4Translate(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+            break;
+        case "scale":
+            Mat4x4Scale(app.transforms[index].mat4x4, values[0], values[1], values[2]);
+            break;
+        case "rotate_x":
+            Mat4x4RotateX(app.transforms[index].mat4x4, values[0]);
+            break;
+        case "rotate_y":
+            Mat4x4RotateY(app.transforms[index].mat4x4, values[0]);
+            break;
+        case "rotate_z":
+            Mat4x4RotateZ(app.transforms[index].mat4x4, values[0]);
+            break;
+        case "shear":
+            Mat4x4ShearXY(app.transforms[index].mat4x4, values[0], values[1]);
+            break;
+    }
 
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
--- scripts/transforms.js
+++ scripts/transforms.js
@@ -1,36 +1,74 @@
 // set values of mat4x4 to the identity matrix
 function Mat4x4Identity(mat4x4) {
-
+    mat4x4.values = [
+        [1,0,0,0],
+        [0,1,0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the translate matrix
 function Mat4x4Translate(mat4x4, tx, ty, tz) {
-    //mat4x4.values = ...
+    mat4x4.values = [
+        [1,0,0,tx],
+        [0,1,0,ty],
+        [0,0,1,tz],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the scale matrix
 function Mat4x4Scale(mat4x4, sx, sy, sz) {
-    //mat4x4.values = ...
+    mat4x4.values = [
+        [sx,0,0,0],
+        [0,sy,0,0],
+        [0,0,sz,0],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the rotate about x-axis matrix
 function Mat4x4RotateX(mat4x4, theta) {
-    //mat4x4.values = ...
+    theta_rad = theta/180 * Math.PI;
+    mat4x4.values = [
+        [1,0,0,0],
+        [0,Math.cos(theta_rad),-Math.sin(theta_rad),0],
+        [0,Math.sin(theta_rad),Math.cos(theta_rad),0],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the rotate about y-axis matrix
 function Mat4x4RotateY(mat4x4, theta) {
-    //mat4x4.values = ...
+    theta_rad = theta/180 * Math.PI;
+    mat4x4.values = [
+        [Math.cos(theta_rad),0,Math.sin(theta_rad),0],
+        [0,1,0,0],
+        [-Math.sin(theta_rad),0,Math.cos(theta_rad),0],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the rotate about z-axis matrix
 function Mat4x4RotateZ(mat4x4, theta) {
-    //mat4x4.values = ...
+    theta_rad = theta/180 * Math.PI;
+    mat4x4.values = [
+        [Math.cos(theta_rad),-Math.sin(theta_rad),0,0],
+        [Math.sin(theta_rad),Math.cos(theta_rad),0,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ];
 }
 
 // set values of mat4x4 to the shear parallel to the xy-plane matrix
 function Mat4x4ShearXY(mat4x4, shx, shy) {
-    //mat4x4.values = ...
+    mat4x4.values = [
+        [1,0,shx,0],
+        [0,1,shy,0],
+        [0,0,1,0],
+        [0,0,0,1]
+    ];
 }
 
 // set the x,y,z values of a 3-component vector
